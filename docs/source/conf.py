374e6dffe8799bc93b6a363c60203a5b9224d947
Dmitry Ryumin <dl_03.03.1991@mail.ru>
2022-12-22 18:57:59 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name): return MagicMock()
 
 MOCK_MODULES = [
-    'colorama', 'prettytable', 'numpy' 'pandas'
+    'colorama', 'prettytable', 'numpy', 'pandas'
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
